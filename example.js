25577fb802255d73b9b609f1860c1e47f602c158
Nick Niemeir <nick.niemeir@gmail.com>
2017-05-25 11:47:35 -0700

    Add a startOffset option
    Allows reading to begin anywhere in the file. I want this so
    I can resuming tailing a file from a saved point,
    and consume data that has been added while the process
    was stopped.

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
 var read = require('./')
 
-var rs = read(fs.openSync(__filename, 'r'), {retry: 100})
+var rs = read(fs.openSync(__filename, 'r'), {retry: 100, startOffset: 0})
 
-rs.on('data', console.log)
+rs.on('data', function (buf) {
+  console.log(buf.toString())
+})
